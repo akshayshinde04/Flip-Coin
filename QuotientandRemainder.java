f46b2a7bb1dafba16bba88f33d6c987864ec15a8
unknown <akshayHVshinde04@gmail.com>
2021-09-29 23:43:45 +0530

    Added: Six_ComputeQuotientandRemainder

---
+++ QuotientandRemainder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class QuotientandRemainder {
+	public static void main(String[] args) {
+		int dividend = 100, divisor = 6;
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+		System.out.println("Quotient = " + quotient);
+		System.out.println("Remainder = " + remainder);
+	}
+}
