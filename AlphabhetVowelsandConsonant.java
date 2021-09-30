1ffc5f0e1e70b93629d998cdf9342590775488e9
unknown <akshayHVshinde04@gmail.com>
2021-09-30 22:47:11 +0530

    Added: Nine_CheckWhetheranAlphabetisVowelorConsonant

---
+++ AlphabhetVowelsandConsonant.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class AlphabhetVowelsandConsonant {
+	public static void main(String[] args) {
+		char ch = 'z';
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+		System.out.println(ch + " is vowel");
+		else
+		System.out.println(ch + " is consonant");
+	}
+}
+     
